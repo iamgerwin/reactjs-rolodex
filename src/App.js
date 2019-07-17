ee3065fb79602c4821a2fa8f0022dc9be948e464
iamgerwin <iamgerwin@aim.com>
2019-07-18 03:14:31 +0800

    add filter functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
-import PropTypes from "prop-types";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
   componentDidMount() {
@@ -17,9 +17,23 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <input
+          type="search"
+          placeholder="search monster"
+          onChange={e =>
+            this.setState({
+              searchField: e.target.value
+            })
+          }
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
